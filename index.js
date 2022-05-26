f75f8560c168c506eeab626e4f1a57b6bacc23bb
Md Aminul Islam Raju <razu2396@gmail.com>
2022-05-27 01:51:17 +0600

    upload all project  files

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+    function buttonfunction(val){
+        form.display.value+= val;
+    }
+    function equal(){
+        form.display.value= form.display .value =eval(form.display.value);
+    }
+    function clearscreen(){
+        form.display.value="";
+    }
